fe1658f6f9188f77b4bc298110c16faf1fc0ba45
25vineeth <vineethsairaj25@gmail.com>
2017-10-29 18:55:30 +0530

    Partial completion

--- vineeth-rms/my-app/src/main/java/com/mycompany/app/App.java
+++ vineeth-rms/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,18 +1,33 @@
 package com.mycompany.app;
 import java.util.*;
-
-public class App 
+public class App extends Thread
 {  
-//Public HashMap<Integer,MenuItem> Dish= new HashMap<Integer,MenuItem>();
-    public static void main( String[] args )
-    {
-        Menu customer = new Menu();
+        Menu customer = new Menu(); 
+public void run(){
+        
+        try{
+        Thread.sleep(5000);
         Scanner in=new Scanner(System.in);
         System.out.print("Give your order (Enter The ItemNO) :");
        // new OrderDish(customer.order(in.nextInt()));   
         int Itemno = in.nextInt();
         new OrderDish(customer.getItemName(Itemno));
-        new OrderDish(customer.getItemPrice(Itemno));        
+        
+        new OrderDish(customer.getItemPrice(Itemno)); 
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }        
+              
+}      
+//Public HashMap<Integer,MenuItem> Dish= new HashMap<Integer,MenuItem>();
+    public static void main( String[] args )
+    {
+       
+     App c1=new App();
+     App c2=new App();
+     c1.start();
+     c2.start();     
     }
     
 }
